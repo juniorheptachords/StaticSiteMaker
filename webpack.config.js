ae68cb1f892e5999517d2086a8127a26fcd09ba6
juniorheptachords <christophe_jourdan@hotmail.fr>
2019-03-23 10:36:19 -0300

    Added license file and header

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+/*
+   _____ _        _   _         _____ _ _         __  __       _             
+  / ____| |      | | (_)       / ____(_) |       |  \/  |     | |            
+ | (___ | |_ __ _| |_ _  ___  | (___  _| |_ ___  | \  / | __ _| | _____ _ __ 
+  \___ \| __/ _` | __| |/ __|  \___ \| | __/ _ \ | |\/| |/ _` | |/ / _ \ '__|
+  ____) | || (_| | |_| | (__   ____) | | ||  __/ | |  | | (_| |   <  __/ |   
+ |_____/ \__\__,_|\__|_|\___| |_____/|_|\__\___| |_|  |_|\__,_|_|\_\___|_|   
+                                                                             
+                                                                             
+Copyright (c) 2019 Christophe Jourdan.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 const path = require('path');
 const fs = require('fs')
